5af60b2cc5a28a1286eab4c9f80b994077c258e3
DarkWater <denis.arushanov@darkcreative.ru>
2026-02-08 15:28:16 +0300

    Update local_precompile.rb

--- lib/local_precompile.rb
+++ lib/local_precompile.rb
@@ -1,1 +1,15 @@
-module LocumBestPractices; end
+module LocalPrecompile; end
+
+# Monkeypatch the File Class with the exists? method
+unless Dir.respond_to?(:exists?)
+  class << Dir
+    alias_method :exists?, :exist?
+  end
+end
+
+# Monkeypatch the Dir Class with the exists? method
+unless File.respond_to?(:exists?)
+  class << File
+    alias_method :exists?, :exist?
+  end
+end
